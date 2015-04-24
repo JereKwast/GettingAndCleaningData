aa1fc352d6050e293366b38f1e97b433085b5f41
aris969 <mondoxgecko@outlook.com>
2015-04-24 13:46:52 -0500

    4/24/2015

--- run_analysis.R
+++ run_analysis.R
@@ -2,17 +2,17 @@
 library("data.table")
 library("dplyr")
 
-setwd("~/R/getdata-projectfiles-UCI HAR Dataset")
+##setwd("~/R/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
 
 ##read data
-features <- read.table("UCI HAR Dataset/features.txt", quote="\"")
-activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("Activity.ID", "Activity.Description"),quote="\"")
+features <- read.table("features.txt", quote="\"")
+activity.labels <- read.table("activity_labels.txt", col.names=c("Activity.ID", "Activity.Description"),quote="\"")
 ##Y_labels <- rename(Y_labels, ActivityID = V1 , ActivityDescription = V2)
 
 ##Test Data
-test.var <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=features[[2]], quote="\"")
-test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names=c("Subject.ID"), quote="\"")
-test.activity <- read.table("UCI HAR Dataset/test/y_test.txt", col.names=c("Activity.ID"), quote="\"")
+test.var <- read.table("X_test.txt", col.names=features[[2]], quote="\"")
+test.subject <- read.table("subject_test.txt", col.names=c("Subject.ID"), quote="\"")
+test.activity <- read.table("y_test.txt", col.names=c("Activity.ID"), quote="\"")
 ##Y_test <- rename(Y_test, ActivityID = V1)
 test.label <- merge(test.activity, activity.labels, by="Activity.ID",all=TRUE)
 
@@ -23,9 +23,9 @@ test.var <- dplyr::bind_cols(test.subject, test.var)
 test.var <- select(test.var, Subject.ID, Activity.ID, Activity.Description, matches("mean"), matches("std"))
 
 ##Training Data
-train.var <- read.table("UCI HAR Dataset/train/X_train.txt", col.names=features[[2]], quote="\"")
-train.subject <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names=c("Subject.ID"), quote="\"")
-train.activity <- read.table("UCI HAR Dataset/train/y_train.txt", col.names=c("Activity.ID"), quote="\"")
+train.var <- read.table("X_train.txt", col.names=features[[2]], quote="\"")
+train.subject <- read.table("subject_train.txt", col.names=c("Subject.ID"), quote="\"")
+train.activity <- read.table("y_train.txt", col.names=c("Activity.ID"), quote="\"")
 train.label <- merge(train.activity, activity.labels, by="Activity.ID",all=TRUE)
 
 train.var <- dplyr::bind_cols(train.label, train.var)
@@ -37,6 +37,12 @@ train.var <- select(train.var, Subject.ID, Activity.ID, Activity.Description, ma
 
 ##Merge Test and Training
 df <- dplyr::bind_rows(test.var, train.var)
+
+nm <- colnames(df)
+write.table(nm, "cols.csv", sep=",")
+
+
+
 
 
 
